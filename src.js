87c3bfa9da0496cf612579e262eeb6485f484dfe
vanshikar <60155140+vanshikar@users.noreply.github.com>
2020-07-05 00:13:05 +0530

    Add files via upload

--- src.js
+++ src.js
@@ -1,54 +1,87 @@
 //when new game button is pressed
 //clearboard
-var board = ['-','-','-','-','-','-','-','-','-'];
+//var board = ['-','-','-','-','-','-','-','-','-'];
+var board = [['-','-','-'],['-','-','-'],['-','-','-']];
+//var board;
 var compfirst = false;
-//clearBoard();
-function setfirst(first)
+var maxdepth= -1;
+var boardSize=3;
+function setFirst(first)
 {
     if(first=='hu') compfirst = false;
     if(first=='co') compfirst = true;
 }
-    
+
+function difficulty(depth){
+    maxdepth=depth;
+    console.log(maxdepth);
+}
+function makeBoard(size){
+    boardSize=size; 
+}
 function clearBoard()
 {
-    
-    //write code to print clean board
-    //code to initialise new board
-        board = ['-','-','-','-','-','-','-','-','-'];
-        let blank=document.getElementById("board");
-        blank.innerHTML='<div id="cell-0" onclick="playermoved(0)"></div><div id="cell-1" onclick="playermoved(1)"></div><div id="cell-2" onclick="playermoved(2)"></div><div id="cell-3" onclick="playermoved(3)"></div><div id="cell-4" onclick="playermoved(4)"></div<div id="cell-5" onclick="playermoved(5)"></div><div id="cell-6" onclick="playermoved(6)"></div><div id="cell-7" onclick="playermoved(7)"></div><div id="cell-8" onclick="playermoved(8)"></div>';
+    //initializing new board
+    for(let i=0;i<boardSize;i++)
+        for(let j=0;j<boardSize;j++)
+            board[i][j]='-';
+    //board = ['-','-','-','-','-','-','-','-','-'];
+    //remove any previous moves
+    let blank=document.getElementById("board");
+    if(boardSize==3)
+        blank.innerHTML='<div id="cell-0" onclick="playermoved(0)"></div><div id="cell-1" onclick="playermoved(1)"></div><div id="cell-2" onclick="playermoved(2)"></div><div id="cell-3" onclick="playermoved(3)"></div><div id="cell-4" onclick="playermoved(4)"></div><div id="cell-5" onclick="playermoved(5)"></div><div id="cell-6" onclick="playermoved(6)"></div><div id="cell-7" onclick="playermoved(7)"></div><div id="cell-8" onclick="playermoved(8)"></div>';
+    console.log('Boardmade');
     //check if starting player is human or computer
     //starting player is human
     // as soon as move is made call return best move function
     //starting player is computer call return best move function
     if(compfirst)
     {
-        var ans = returnbestmove();
-        board[ans] = 'x';
-        //print this move
+        //for first move, randomly choosing between [0,2,4,6,8]
+        var arr=[0,2,4,6,8];
+        var ans=arr[Math.floor(Math.random()*arr.length)];
+        //board[ans] = 'x';
+        board[Math.floor(ans/boardSize)][ans%boardSize]='x';
         printMove(ans,'x');
+        console.log(board);
     }
     
 }
     
+function gameOver(res)
+{
+    if(res!='t'){
+        for(let i=0;i<9;i++)
+    document.getElementById("cell-"+i).onclick=null;
+    drawWinningLine(res[1],res[2]+1);
+    }
+}
+
+function drawWinningLine({ direction, row }) {
+	let board = document.getElementById("board");
+	board.className = `${direction}${row}`;
+	setTimeout(() => { board.className += ' full'; }, 50);
+}
+
 function playermoved(move)
 {
-    
-    console.log(board);
-    board[move] = 'o';
-    //print player's move
+    console.log(move/boardSize);
+    board[Math.floor(move/boardSize)][Math.floor(move%boardSize)] = 'o';
     printMove(move,'o');
-    //check if it is draw or if anyone won
-    var ans = returnbestmove();
-    board[ans] = 'x';
-    printMove(ans,'x');
-    console.log(ans);
-    console.log(board);
-    //print this move
-    
+    var temp = evalBoard();
+    if(temp!='n') gameOver(temp);
+    else
+    {
+        var ans = returnbestmove();
+        board[Math.floor(ans/boardSize)][ans%boardSize] = 'x';
+        printMove(ans,'x');
+        console.log(board);
+        var temp1 = evalBoard();
+        console.log(temp1);
+        if(temp1!='n') gameOver(temp1);
+    }    
 }
-    
-    
+
 //function printMove
 function printMove(move,symbol){
     var img=document.createElement("img");
@@ -57,9 +90,10 @@ function printMove(move,symbol){
     else if(symbol=='x')
         img.src="images/robotface.png";
     let place=document.getElementById("cell-"+move);
-    img.style.width='50%';
-    img.style.height='auto';
+    img.style.width='70%';
     place.appendChild(img);
+    place.style.textAlign="center";
+    place.onclick=null;
 }
 //function that is called everytime player makes a move
 //checks if board is full
@@ -72,25 +106,26 @@ function returnbestmove()
     var move;
     var score = -10000;
     //board is global
-    for(var i =0;i<9;i++)
-    {
-        //check if move is possible at i
-        if(board[i]=='-')
+    for(var i =0;i<3;i++)
+        for(var j=0;j<3;j++)
         {
-            //make the move
-            board[i] = 'x';
-            //evaluate score on making this move
-            var temp = returnScore(false);
-            if(temp>score)
+            //check if move is possible at i
+            if(board[i][j]=='-')
             {
-                //update this move as the best move so far
-                score = temp;
-                move = i;
+                //make the move
+                board[i][j] = 'x';
+                //evaluate score on making this move
+                var temp = returnScore(false,0);
+                if(temp>score)
+                {
+                    //update this move as the best move so far
+                    score = temp;
+                    move = 3*i+j;
+                }
+                //undo this move
+                board[i][j] = '-';
             }
-            //undo this move
-            board[i] = '-';
         }
-    }
     return move;
     
 }
@@ -100,47 +135,55 @@ function evalBoard()
     //return x if x is winner
     //return o if o is winner
     //else return n
-    //check all rows
-    for(var i = 0;i<9;i+=3)
-    {
-        if(board[i]==board[i+1] && board[i+1]==board[i+2]) return board[i];
+    //check for ties
+    var isfull = true;
+    for(var i =0;i<3;i++)
+        for(var j=0;j<3;j++)
+            if(board[i][j]=='-') isfull=false;
+    if(isfull) return 't';
+    
+    for(let i=0;i<3;i++){
+        //rows
+        if(board[i][0]==board[i][1] && board[i][1]==board[i][2]){
+            if(board[i][0]!='-') return board[i][0];
+        }
+        //cols
+        if(board[0][i]==board[1][i] && board[1][i]==board[2][i]){
+            if(board[0][i]!='-') return board[0][i];
+        }
     }
-    //check all columns
-    for(var i = 0;i<3;i++)
-    {
-        if(board[i]==board[i+3] && board[i+3]==board[i+6]) return board[i];
-    }
+
     //check 2 diagonals
-    if(board[0]==board[4] && board[4]==board[8]) return board[0];
-    if(board[2]==board[4] && board[4]==board[6]) return board[2];
+    if(board[0][0]==board[1][1] && board[1][1]==board[2][2]) 
+    {
+        if(board[0][0]=='o') return 'o';
+        if(board[0][0]=='x') return 'x';
+    }
     
-    return 'tie';
+    if(board[0][2]==board[1][1] && board[1][1]==board[2][0]) 
+    {
+        {
+            if(board[0][2]=='o') return 'o';
+            if(board[0][2]=='x') return 'x';
+        }
+    }
+    
+    return 'n';
 }
-    
-    
-    
-    
-    
-function returnScore(isMax)
+  
+function returnScore(isMax,depth)
 {
     //check if either player is winner
     //return -10 if o is winner
     //return +10 if x is winner
     //return 0 if it is a draw
-    var isfull = true;
-    for(var i=0;i<9;i++)
-    {
-        if(board[i]=='-') {
-            isfull = false;
-            break;
-        }
-    }
-    if(isfull) return 0;
     
     //check if winner
     var temp1 = evalBoard();
-    if(temp1=='x') return 10;
-    if(temp1=='o') return -10;
+    if(temp1[0]=='t') return 0;
+    if(temp1[0]=='x') return 10-depth;
+    if(temp1[0]=='o') return -10+depth;
+    if(depth==maxdepth) return 0;
     
     
     
@@ -149,33 +192,35 @@ function returnScore(isMax)
     {
         //make all possible moves and return highest possible score
         var score = -1000;
-        for(var i = 0;i<9;i++)
-        {
-            if(board[i]=='-')
+        for(var i =0;i<3;i++)
+            for(var j=0;j<3;j++)
             {
-                board[i]='x';
-                var temp = returnScore(false);
-                score = Math.max(score,temp);
-                board[i] = '-';
-    
+                if(board[i][j]=='-')
+                {
+                    board[i][j]='x';
+                    var temp = returnScore(false,depth+1);
+                    score = Math.max(score,temp);
+                    board[i][j] = '-';
+        
+                }
             }
-        }
         return score;
     }
     else
     {
         var score = 1000;
-        for(var i = 0;i<10;i++)
-        {
-            if(board[i]=='-')
+        for(var i =0;i<3;i++)
+        for(var j=0;j<3;j++)
             {
-                board[i]='o';
-                var temp = returnScore(true);
-                score = Math.min(score,temp);
-                board[i] = '-';
-    
-            } 
-        }
+                if(board[i][j]=='-')
+                {
+                    board[i][j]='o';
+                    var temp = returnScore(true,depth+1);
+                    score = Math.min(score,temp);
+                    board[i][j] = '-';
+        
+                } 
+            }
         return score;
     }
     
